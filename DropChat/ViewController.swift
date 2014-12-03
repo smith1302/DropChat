2030fcc80b980490e74290a53aafe253b1669245
Eric Smith <smith1302@Erics-Mac.local>
2014-12-03 15:30:16 -0800

    Initial Commit

---
+++ DropChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropChat
+//
+//  Created by Eric Smith on 12/3/14.
+//  Copyright (c) 2014 Eric Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
